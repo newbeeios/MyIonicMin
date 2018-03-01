535c58580652c52f231a44bbe871ae62fe497121
Bharadwaja Gajula <bharadwajagajula@Bharadwajas-MacBook-Pro.local>
2018-02-28 20:44:48 -0500

    reports

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -1,12 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Chart } from 'chart.js';
-/**
- * Generated class for the ReportsPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
